7855070247fbb8a759c3819e279e476fe1632496
artem ivanov <nrth5.dkt9@gmail.com>
2025-06-20 21:16:45 +0300

    initial commit

---
+++ src/main/java/by/northdakota/subcriptionservice/SubcriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package by.northdakota.subcriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubcriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubcriptionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/northdakota/subcriptionservice/SubcriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.northdakota.subcriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubcriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
